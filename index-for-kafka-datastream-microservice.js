889d59dab8ee25f584831789c28f5b977b17a0c2
Colin Ryan <colin.ryan@davranetworks.com>
2018-07-04 20:34:27 +0100

    Create index-for-kafka-datastream-microservice.js

---
+++ index-for-kafka-datastream-microservice.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const kafka = require('kafka-node');
+const fs = require("fs");
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('connecthing.io node microservice!');
+});
+
+const SERVER_PORT = 8080;
+app.listen(SERVER_PORT, function () {
+  console.log('connecthing.io node microservice listening on port ' + SERVER_PORT + '!');
+});
+
+const ConsumerGroup = kafka.ConsumerGroup;
+const consumer = new ConsumerGroup({
+    kafkaHost: process.env.KAFKA_HOST,
+    groupId: process.env.KAFKA_CONSUMER_GROUP_ID,
+    fromOffset: "earliest",
+    ssl: true,
+    sslOptions: {
+        key: fs.readFileSync('/etc/davra/tls/tls.key'),
+        cert: fs.readFileSync('/etc/davra/tls/tls.crt'),
+        ca: [ fs.readFileSync("/etc/davra/tls/ca-cert") ],
+        rejectUnauthorized: false
+    } 
+  },
+  [ process.env.IoT_DATA_TOPIC_NAME ]);
+
+consumer.on('error', function (err) {
+	console.error("Kafka consumer group reporting error", err);
+	console.error(err.stack);  
+	process.exit(1);
+});
+
+let count=0;
+consumer.on("message", function(message){
+    if(count++ % 1000 === 0){
+        console.dir(message);
+    }
+});
+
