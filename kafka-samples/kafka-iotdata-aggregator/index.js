b7b75ece75b1a6451fe32651dfa2259accd86cd4
colindavra <colin.ryan@davranetworks.com>
2020-07-14 12:22:11 +0000

    Sample aggregation code

---
+++ kafka-samples/kafka-iotdata-aggregator/index.js
@@ -0,0 +1,65 @@
+
+const express = require('express');
+const kafka = require('kafka-node');
+const fs = require("fs");
+const app = express();
+
+const aggregator = require("./aggregator.js");
+
+
+
+var config = {
+    DEBUG: false,
+    redis: {
+        host: "redis.default",
+        port: 6379
+    },
+    bucket_size: 60 * 60 * 1000,
+    bucket_ttl: 24 * 60 * 60 * 1000
+};
+
+
+app.get('/', function (req, res) {
+    res.send('Davra node microservice!');
+});
+
+const SERVER_PORT = 8080;
+app.listen(SERVER_PORT, function () {
+    console.log('Davra node microservice listening on port ' + SERVER_PORT + '!');
+});
+
+aggregator.init(config);
+
+
+const ConsumerGroup = kafka.ConsumerGroup;
+const consumer = new ConsumerGroup({
+    kafkaHost: process.env.KAFKA_HOST,
+    groupId: process.env.KAFKA_CONSUMER_GROUP_ID,
+    fromOffset: "earliest",
+    ssl: true,
+    sslOptions: {
+        key: fs.readFileSync('/etc/davra/tls/tls.key'),
+        cert: fs.readFileSync('/etc/davra/tls/tls.crt'),
+        ca: [ fs.readFileSync("/etc/davra/tls/ca-cert") ],
+        rejectUnauthorized: false
+    } 
+  },
+  [ process.env.IoT_DATA_TOPIC_NAME ]);
+
+consumer.on("message", function(message){
+    //console.log("Message: %o", message);
+    aggregator.processMessage(message);
+});
+
+
+consumer.on("error", function(err){
+    console.error('Kafka broker error', err);
+    process.exit(1);  
+});
+
+
+/*Instruct all aggregators to flush to iotdata*/
+setInterval(function(){
+    aggregator.writeToStore().catch((err) => {console.error("Error writing to iotdata", err);});
+}, 2500);
+
