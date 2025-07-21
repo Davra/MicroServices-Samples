5577718e1104ef59fb93481ed97e18b038f90a34
colindavra <colin.ryan@davranetworks.com>
2025-07-21 13:16:09 +0000

    updating sample code to reflect revised best practices

--- simple-microservice/index.js
+++ simple-microservice/index.js
@@ -1,6 +1,16 @@
+const fs = require("fs");
+const axios = require('axios');
 const express = require('express');
+
 const app = express();
-const API_HOST = process.env.DAVRA_API_HOST
+const API_HOST = process.env.DAVRA_API_HOST || 'http://api.connecthing';
+console.log(`Using ${API_HOST} to make davra calls`);
+
+/* Read-in the api token granted to the microservice 
+This token is mounted into the container running your custom microservice by the Davra Platform
+The token identifies the microservice entity to the Davra Platform when making api calls from the 
+microservice.*/
+const token = fs.readFileSync("/etc/connecthing-api/token", {encoding: 'utf-8'}).trim();
 
 // An example of microservice responding to incoming HTTP requests (GET)
 // You can test this by clicking the "Send" button on your far right of the code editor
@@ -9,39 +19,32 @@ app.get('/', function (req, res) {
     console.log('Http call seen arriving at this microservice');
 });
 
-
-app.get('/test', function (req, res) {
-    res.send('Davra node microservice here!');
-    console.log('Http call seen arriving at this microservice /test : ', req);
-});
-
-app.get('/davra', function (req, res) {
-    res.send('Davra node microservice here!');
-    console.log('Http call seen arriving at this microservice /test : ', req);
-});
-
-app.get('/hello', function (req, res) {
-    res.send('Hello World!');
-    console.log('Http call seen arriving at this microservice /hello');
-});
-
-
-// Example of making an API call to another microservice inside Connecthing
-var api = require("@connecthing.io/connecthing-api");
-api.request({
-    url: API_HOST+"/api/v1/devices",
-    callback: function(err, response, body){
-        console.log('Http call was made to /api/v1/devices and got back: ', body);
+/* An example of an api call being exposed by a custom microservice whi */
+app.get('/davra', async function (req, res) {
+    try {
+        const opts = {
+            method: "GET",
+            baseURL: API_HOST,
+            url: '/api/v1/devices',
+            params: {
+                start: 0,
+                limit: 1
+            },
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        };
+        const result = await axios(opts);
+        res.send(`Total device records found: ${result.data.totalRecords}`);
+    } catch (err) {
+        console.error('Error processing api call', err);
+        res.status(500).send(`Failed to make api call: ${err.message}`);
     }
 });
+
 
 // Set the microservice running and listening for incoming HTTP requests
 const SERVER_PORT = 8080;
 app.listen(SERVER_PORT, function () {
     console.log('Davra node microservice listening on port ' + SERVER_PORT);
 });
-
-// Example of repeatedly writing to the logs, thus demonstrating microservice is running
-setInterval(function() {
-    console.log('microservice running. Timestamp is:' + new Date());
-}, 3000)
